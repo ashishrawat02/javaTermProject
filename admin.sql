5be596d06078c3c47f97c10b800c73ca348b7d59
ashish <ashish@ashish-pc>
2015-04-18 21:07:38 -0400

    added admin and student sql database file

---
+++ admin.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2015 at 04:58 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `college`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `username` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', 'admin');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- student.sql
+++ student.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2015 at 02:32 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Generation Time: Apr 19, 2015 at 03:06 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,28 +27,44 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `student` (
-  `studentId` int(10) NOT NULL AUTO_INCREMENT,
-  `fisrtName` varchar(50) NOT NULL,
-  `nameLast` varchar(50) NOT NULL,
+`studentId` int(10) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
   `course` varchar(225) NOT NULL,
   `duration` int(10) NOT NULL,
   `address` varchar(225) NOT NULL,
   `birthDate` date NOT NULL,
-  `phoneNumber` int(50) NOT NULL,
-  PRIMARY KEY (`studentId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=106 ;
+  `phoneNumber` int(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10652 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`studentId`, `fisrtName`, `nameLast`, `course`, `duration`, `address`, `birthDate`, `phoneNumber`) VALUES
-(101, 'Ashish', 'Rawat', 'IPRC', 2, 'Devine Street', '1991-03-02', 2114024461),
-(102, 'Neha', 'Sahota', 'IPRC', 2, 'Erindale', '1992-09-06', 2124024465),
-(103, 'Kuldeep', 'Singh', 'IPRC', 2, 'Finch', '1990-05-12', 2124468123),
-(104, 'Bino', 'Sammule', 'IPRC', 2, 'Finch', '1990-12-26', 2116659456),
-(105, 'Revati', 'Rajendaran', 'IPRC', 2, 'College ave', '1991-09-24', 2116698024);
+INSERT INTO `student` (`studentId`, `firstName`, `lastName`, `course`, `duration`, `address`, `birthDate`, `phoneNumber`) VALUES
+(101, 'Ashish', 'Rawat', 'IPRC', 3, '54E,849 Devine Street', '1991-03-02', 2114024455),
+(102, 'revati', 'radendra', 'itp', 2, 'college ve', '1991-03-02', 2114024455),
+(1015, 'joe', 'barkhouse', 'greenhhouse', 50, 'telfer', '1989-03-02', 13514601);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+ ADD PRIMARY KEY (`studentId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+MODIFY `studentId` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10652;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
